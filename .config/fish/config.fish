7e888f00b70e3ef81b3674d635e4c0d54a42e5d7
Kahlil Wehmeyer <kahlilwehmeyer@gmail.com>
2020-12-07 15:07:59 -0500

    spice for spotify

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 
+
 #  ████ ██         ██                                    ████ ██        
 # ░██░ ░░         ░██                                   ░██░ ░░   █████ 
 #██████ ██  ██████░██         █████   ██████  ███████  ██████ ██ ██░░░██
@@ -13,3 +14,6 @@
 
 alias header="~/.config/bash_scripts/header.sh"
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+export SPICETIFY_INSTALL="/home/kahlil/spicetify-cli"
+export PATH="$SPICETIFY_INSTALL:$PATH"
