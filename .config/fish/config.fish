24b89aea150042ab117aff5ebcde45f3e7ac3b1e
Kahlil Wehmeyer <kahlilwehmeyer@gmail.com>
2020-12-07 16:50:17 -0500

    colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,8 +12,11 @@
 # Author: Kahlil Wehmeyer
 # Github: github.com/khanzi
 
+cat ~/.cache/wal/sequences
+
 alias header="~/.config/bash_scripts/header.sh"
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 export SPICETIFY_INSTALL="/home/kahlil/spicetify-cli"
 export PATH="$SPICETIFY_INSTALL:$PATH"
+
