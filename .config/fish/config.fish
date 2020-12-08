bd902c612871fd5f3b59816188f435f7fc2513f5
Kahlil Wehmeyer <kahlilwehmeyer@gmail.com>
2020-12-07 23:22:13 -0500

    small changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,3 +20,7 @@ alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 export SPICETIFY_INSTALL="/home/kahlil/spicetify-cli"
 export PATH="$SPICETIFY_INSTALL:$PATH"
 
+alias mon2cam="deno run --unstable -A -r -q https://raw.githubusercontent.com/ShayBox/Mon2Cam/master/src/mod.ts"
+
+export DENO_INSTALL="/home/kahlil/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
