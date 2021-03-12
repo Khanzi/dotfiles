e75d913ba753d199cdce9d18cc593c074b867f03
Kahlil Wehmeyer <kahlilwehmeyer@pm.me>
2021-03-12 08:13:29 -0500

    small changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -26,5 +26,6 @@ alias screen_clip="import png:- | xclip -selection clipboard -t image/png"
 alias vi=nvim
 alias sudo=doas
 alias mine=$HOME/AUR/xmrig-6.9.0/xmrig
+alias jul=kitty julia
 
 set PATH $HOME/.config/bash_scripts $PATH
